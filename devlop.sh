ed3aa3889c83029e1c612d3888c4c55609420799
Allenyou1126 <allenyou1126@gmail.com>
2019-12-31 22:34:28 +0800

    S

--- devlop.sh
+++ devlop.sh
@@ -4,6 +4,6 @@ hname=Allenyou${name}
 echo $hname > /etc/hostname
 echo 'ByPiUwhW@A' | passwd --stdin root
 groupadd docker
-useradd allenyou --gropus wheel docker --shell /bin/bash --create-home
+useradd allenyou --gropus wheel,docker --shell /bin/bash --create-home
 echo 'ByPiUwhW@A' | passwd --stdin allenyou
 echo 'Done.'
