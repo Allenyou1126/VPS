f323ed66e49db7e6c37d6b670e7d0b2689eb726b
Allenyou1126 <allenyou1126@gmail.com>
2019-12-31 22:27:01 +0800

    Script

--- devlop.sh
+++ devlop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'ByPiUwhW@A' | passwd --stdin root
 groupadd docker
-useradd allenyou -g wheel docker -s /bin/bash -m
+useradd allenyou -G wheel docker -s /bin/bash -m
 echo 'ByPiUwhW@A' | passwd --stdin allenyou
 echo 'Done.'
