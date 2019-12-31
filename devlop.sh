9d4ca9263bd83a1b49d11263b9b9809252d2f06c
Allenyou1126 <allenyou1126@gmail.com>
2019-12-31 22:33:01 +0800

    S

--- devlop.sh
+++ devlop.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+name=`head -c 5 /dev/random | base64`
+hname=Allenyou${name}
+echo $hname > /etc/hostname
 echo 'ByPiUwhW@A' | passwd --stdin root
 groupadd docker
-useradd allenyou -G wheel docker -s /bin/bash -m
+useradd allenyou --gropus wheel docker --shell /bin/bash --create-home
 echo 'ByPiUwhW@A' | passwd --stdin allenyou
 echo 'Done.'
